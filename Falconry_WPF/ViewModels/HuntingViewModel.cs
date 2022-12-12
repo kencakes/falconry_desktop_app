60fbb10f3878276ec1c7977cbf068df0f4044b56
kencakes <vansaet.kendra@gmail.com>
2022-12-13 00:14:34 +0100

    Sidepanel menu

---
+++ Falconry_WPF/ViewModels/HuntingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Falconry_WPF.ViewModels
+{
+    internal class HuntingViewModel
+    {
+    }
+}
---
+++ Falconry_WPF/ViewModels/NavigationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Falconry_WPF.Utilities;
+using System.Windows.Input;
+using Falconry_WPF.Views;
+
+namespace Falconry_WPF.ViewModels
+{
+    class NavigationViewModel: ViewModelBase
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+        
+        // For the button bindings
+        public ICommand HomeCommand { get; set; }
+        public ICommand BirdCommand { get; set; }
+        public ICommand HuntingCommand { get; set; }
+        public ICommand LogbookCommand { get; set; }
+        // TODO: Register, Login, Reset password
+
+        private void Home(object obj) => CurrentView = new HomeViewModel();
+        private void Bird(object obj) => CurrentView = new BirdViewModel();
+        private void Hunting(object obj) => CurrentView = new HuntingViewModel();
+        private void Logbook(object obj) => CurrentView = new LogbookViewModel();
+        
+
+        public NavigationViewModel()
+        {
+            
+            
+            HomeCommand = new RelayCommand(Home);
+            BirdCommand = new RelayCommand(Bird);
+            HuntingCommand = new RelayCommand(Hunting);
+            LogbookCommand = new RelayCommand(Logbook);
+            
+            // Startup page
+            CurrentView = new HomeView();
+        }
+    }
+}
