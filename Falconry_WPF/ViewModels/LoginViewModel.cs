9c8888bdd07812b0a3cf89d71772fe4e13bb1c7e
kencakes <vansaet.kendra@gmail.com>
2022-12-17 17:53:37 +0100

    Changed startup window

--- Falconry_WPF/ViewModels/LoginViewModel.cs
+++ Falconry_WPF/ViewModels/LoginViewModel.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
+using System.Windows.Data;
 using Falconry_WPF.Data;
 using Falconry_WPF.Models;
 using GalaSoft.MvvmLight.Command;
+using MessageBox = System.Windows.MessageBox;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Falconry_WPF.ViewModels
@@ -14,27 +13,40 @@ namespace Falconry_WPF.ViewModels
     public class LoginViewModel
     {
         public RelayCommand SubmitCommand { get; set; }
+        public RelayCommand ResetPasswordCommand { get; set; }
         public RelayCommand RegisterCommand { get; set; }
 
         public string Username { get; set; }
         public string Password { get; set; }
+        public User User { get; set; }
+
 
         public LoginViewModel()
         {
             SubmitCommand = new RelayCommand(Login);
+            ResetPasswordCommand = new RelayCommand(ResetPassword);
             RegisterCommand = new RelayCommand(Register);
+            
+            // Initialise database on startup if it doens't exist
+            DatabaseFacade facade = new DatabaseFacade(new DataContext());
+            facade.EnsureCreated();
         }
-        
+
         private void Login()
         {
             using (DataContext context = new DataContext())
             {
                 bool userfound = context.Users.Any(user => user.Username == Username && user.Password == Password);
 
-                // TODO: Fix mass login
                 if (userfound)
                 {
-                    MessageBox.Show("You have successfully been logged in!", "Success", MessageBoxButton.OK);
+                    /*
+                    User user = new User();
+                    user = context.Users.Find(1);
+
+                    Views.MainView view = new Views.MainView();
+                    view.Show();
+                    */
                 }
                 else
                 {
@@ -42,22 +54,21 @@ namespace Falconry_WPF.ViewModels
                 }
             }
         }
-        
+
+        private void ResetPassword()
+        {
+            ResetPasswordViewModel vm = new ResetPasswordViewModel();
+            Views.ResetPasswordView view = new Views.ResetPasswordView();
+            view.DataContext = vm;
+            view.Show();
+        }
+
         private void Register()
         {
-            using (DataContext context = new DataContext())
-            {
-                if (Username != null && Password != null)
-                {
-                    // TODOl Unique username, u can make multiple accs with same name now
-                    context.Users.Add(new User() { Username = this.Username, Password = this.Password });
-                    context.SaveChanges();
-                }
-                else
-                {
-                    MessageBox.Show("Not every field has been filled in", "Error");
-                }
-            }
+            RegisterViewModel vm = new RegisterViewModel();
+            Views.RegisterView view = new Views.RegisterView();
+            view.DataContext = vm;
+            view.Show();
         }
     }
 }
--- Falconry_WPF/ViewModels/NavigationViewModel.cs
+++ Falconry_WPF/ViewModels/NavigationViewModel.cs
@@ -4,6 +4,7 @@ using Falconry_WPF.Utilities;
 using System.Windows.Input;
 using Falconry_WPF.Views;
 using Falconry_WPF.Data;
+using Falconry_WPF.Models;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
 
@@ -12,34 +13,7 @@ namespace Falconry_WPF.ViewModels
     class NavigationViewModel: ViewModelBase
     {
         private object _currentView;
-        /*
-        private Visibility _showuser;
-        private Visibility _showsignin;
-        
-        public Visibility ShowSignIn
-        {
-            get 
-            {
-                return _showsignin;
-            }
-            set {
-                _showsignin = value; 
-                OnPropertyChanged("ShowSignIn");
-            }
-        }
-
-        public Visibility ShowUser
-        {
-            get 
-            {
-                return _showuser;
-            }
-            set {
-                _showuser = value; 
-                OnPropertyChanged("ShowUser");
-            }
-        }
-        */
+        //public User User { get; set; }
 
         public object CurrentView
         {
@@ -59,40 +33,27 @@ namespace Falconry_WPF.ViewModels
         public ICommand BirdCommand { get; set; }
         public ICommand HuntingCommand { get; set; }
         public ICommand LogbookCommand { get; set; }
-        public ICommand LoginCommand { get; set; }
-
-
-        // TODO: Register, Reset password
+        
 
         private void Home(object obj) => CurrentView = new HomeViewModel();
         private void Bird(object obj) => CurrentView = new BirdViewModel();
         private void Hunting(object obj) => CurrentView = new HuntingViewModel();
         private void Logbook(object obj) => CurrentView = new LogbookViewModel();
-        private void Login(object obj) => CurrentView = new LoginViewModel();
-        
+
 
         public NavigationViewModel()
         {
+            //this.User = user;
+            
             HomeCommand = new RelayCommand(Home);
             BirdCommand = new RelayCommand(Bird);
             HuntingCommand = new RelayCommand(Hunting);
             LogbookCommand = new RelayCommand(Logbook);
-            LoginCommand = new RelayCommand(Login);
-            
+
             // Startup page
             CurrentView = new HomeView();
-
-            // Initialise database on startup if it doens't exist
-            DatabaseFacade facade = new DatabaseFacade(new DataContext());
-            facade.EnsureCreated();
-            
-            // Auth visibility
-            
         }
-
-        public void AuthVisibility()
-        {
-            
-        }
+        
+        
     }
 }
